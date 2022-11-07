304a4b6fdb70a5a87dd6b2f033dc475e791c301d
DavidKng <David_Kng@hotmail.com>
2022-11-07 20:49:10 -0300

    actualiación de los modelos

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -4,6 +4,37 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("user", {
-
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    age: {
+     type: DataTypes.INTEGER,
+     allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    range: {
+      type: DataType.ENUM("admin", "usuario", "refugio"),
+      defaultValue: "usuario"
+    }
   });
 };
