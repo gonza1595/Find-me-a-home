4b46f59392cf6c902645a9c614e46af52b103eae
diegofvaz <106205927+diegofvaz@users.noreply.github.com>
2022-11-08 16:21:46 -0300

    create mascotas (#15)

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,10 @@
 const { Router } = require("express");
+const mascotas = require('./mascotas');
 
 const router = Router();
 
 // Configurar los routers
+
+router.use('/mascotas', mascotas);
 
 module.exports = router;
