d78e55fda2a25a227bd9557995a5aa149d0208e3
diegofvaz <diegofvaz@gmail.com>
2022-11-08 16:19:20 -0300

    create mascotas

---
+++ api/src/routes/mascotas.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const router = Router();
+const { Pet } = require('../db')
+
+
+router.post('/', async (req, res, next) => {
+
+    const { nombre, descripcion, edad, imagen, tamaño, raza, sexo, especie } = req.body
+       
+    try {
+        let nuevaMascota = await Pet.create ({ 
+            nombre,
+            descripcion,
+            edad,
+            imagen,
+            tamaño,
+            raza,
+            sexo,
+            especie
+        })
+        res.status(200).send(nuevaMascota)
+    } catch(error) {
+        next(error)
+    }
+})
+
+module.exports = router;
