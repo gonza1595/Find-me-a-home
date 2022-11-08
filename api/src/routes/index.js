27b3105418cfeceef4623c1a9ebd9ef69519dd8c
Julian Soto <juliansoto015@gmail.com>
2022-11-08 19:59:14 -0300

    creacion ruta getMascotasById

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,11 @@
-const { Router } = require("express");
+const {Router} = require('express');
 const mascotas = require('./mascotas');
-
+const getMascotasById = require('./getMascotaById.js');
 const router = Router();
 
 // Configurar los routers
 
 router.use('/mascotas', mascotas);
+router.use('/mascotas', getMascotasById);
 
 module.exports = router;
