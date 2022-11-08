b1a6a81b22ab9b06a5a6cf7f200a44337b416793
carolinambb <carolinariveiro50@gmail.com>
2022-11-08 18:03:20 -0300

    componente NavBar

---
+++ client/src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () =>{
+    return(
+        <nav>
+            <ul>
+            <h2>Find me a HOME</h2>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Quienes somos</a></li>
+            <li><a href="">Productos</a></li>
+            <li><a href="">Noticias</a></li>
+            <li><a href="">Adopta!</a></li>
+            <li><a href="">Iniciar sesion</a></li>
+            </ul>
+ </nav>
+    )
+}
+
+export default NavBar
