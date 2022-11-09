e55b1ab85ba3842e3b6a43d0fddf22913405bbe2
Julian Soto <juliansoto015@gmail.com>
2022-11-09 11:53:41 -0300

    creacion ruta para buscar productos por ID y actualizacion del index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const {Router} = require('express');
 const mascotas = require('./mascotas');
 const getMascotasById = require('./getMascotaById.js');
-const getAllProducts = require("./productos");
+const getProductosById = require('./getProductosById');
+const getAllProducts = require('./productos');
 const router = Router();
 
 // Configurar los routers
 
 router.use('/mascotas', mascotas);
 router.use('/mascotas', getMascotasById);
-router.use("/productos", getAllProducts);
+router.use('/productos', getAllProducts);
+router.use('/productos', getProductosById);
 module.exports = router;
