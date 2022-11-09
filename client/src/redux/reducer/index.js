ec58f6b5ba873bb89f420477c0cf29aeacb813b5
Mauri Ale <mauri13jarita@gmail.com>
2022-11-09 12:06:36 -0300

    cambio a redux normal

---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,19 @@
+
+const initialState = {
+    mascotas : [],
+    productos : [],
+    detalle : {},
+    
+
+    //agregar mas estados si se requiere...
+}
+
+function rootReducer(state=initialState, action){
+
+    switch(action.type){
+
+        //case...
+
+    }
+
+}
