cd778fbc7000bd9b672380f1878a140ec37522a8
MauriJara312 <mauri13jarita@gmail.com>
2022-11-10 11:30:39 -0300

    cambios anteriores

---
+++ client/src/componentes/HomeProductos/HomeProductos.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {Link} from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+import SearchBar from "../SearchBar/SearchBarProducto";
+import "./HomeProductos.css"
+import {traerProductos} from "../../redux/actions/index.js"
+//importar paginado cuando este disponible
+//importar carta de producto cuando este disponible
+
+
+export default function HomeProductos (){
+
+    const dispatch = useDispatch()
+
+    
+    const productos = useSelector((state)=>state.productos)
+
+    useEffect(() => {
+        dispatch(traerProductos());
+      }, [dispatch]);
+
+    return (
+<h1>{productos}</h1>
+    )
+}
