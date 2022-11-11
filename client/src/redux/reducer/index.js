3d14a22183a256665855c6ba4147d4e206c46945
gonza1595 <aveirogonzalo3cnp@gmail.com>
2022-11-11 02:01:40 -0300

    algunos cambios arreglados

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -130,45 +130,4 @@ function rootReducer(state = initialState, action) {
   }
 }
 
-function rootReducer(state = initialState, action) {
-  switch (action.type) {
-    case "BUSCAR_POR_NOMBRE_MASCOTA":
-      return {
-        ...state,
-        mascotas: action.payload,
-      };
-    case "BUSCAR_POR_NOMBRE_PRODUCTO":
-      return {
-        ...state,
-        productos: action.payload,
-      };
-
-    case "DETALLE_MASCOTA":
-      return {
-        ...state,
-        detalle: action.payload,
-      };
-
-    case "LIMPIAR_ESTADO_DETALLE":
-      return {
-        ...state,
-        detalle: {},
-      };
-
-    case "TRAER_PRODUCTOS":
-      return {
-        ...state,
-        productos: action.payload,
-      };
-
-    case "DETALLE_PRODUCTO":
-      return {
-        ...state,
-        detalle: action.payload,
-      };
-    default:
-      return state;
-  }
-}
-
 export default rootReducer;
