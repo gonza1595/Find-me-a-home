eb95de93319e09463696770bd34434302bbe591a
MauriJara312 <104162299+MauriJara312@users.noreply.github.com>
2022-11-23 20:03:03 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-const fileUpload = require('express-fileUpload')
+const fileUpload = require("express-fileUpload")
 
 require("./db.js");
 
