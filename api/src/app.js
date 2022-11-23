1c2192e61ccc61ea65c84e238f29a2ca4afc2b8a
MauriJara312 <104162299+MauriJara312@users.noreply.github.com>
2022-11-23 20:13:58 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-const fileUpload = require("express-fileUpload")
+//const fileUpload = require("express-fileUpload")
 
 require("./db.js");
 
@@ -25,10 +25,12 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-server.use(fileUpload({
+
+/*server.use(fileUpload({
   useTempFiles : true,
   tempFileDir : './uploads'
-}));
+}));}*/
+
 server.use("/", routes);
 
 // Error catching endware.
